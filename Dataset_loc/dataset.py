81b521b8e7563f75a1b7b6b92a601454760d1ac1
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-03-26 06:00:12 +0200

    new dataset added xx

--- Dataset_loc/dataset.py
+++ Dataset_loc/dataset.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def dataset():
     # return pd.read_csv('Dataset_loc/dataset/sensor_maintenance_data.csv')
-    # return pd.read_csv('Dataset_loc/dataset/dataset_data.csv')
-    return pd.read_csv('Dataset_loc/dataset/motorcycle_sensor_data.csv')
+    return pd.read_csv('Dataset_loc/dataset/dataset_data.csv')
+    # return pd.read_csv('Dataset_loc/dataset/motorcycle_sensor_data.csv')
     # return pd.read_csv('Crop_data/dataset/Plant_Parameters.csv')
